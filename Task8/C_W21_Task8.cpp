f3f97231dfae06de436f40af935f6dbc0fd895e9
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-05-07 12:07:48 +0300

    Add files via upload

---
+++ Task8/C_W21_Task8.cpp
@@ -0,0 +1,29 @@
+﻿//Write a function that takes a pointer to an integer
+//a numberand returns its inverse.
+
+#include <iostream>
+
+using namespace std;
+
+void revers(int *pnum);
+
+int main()
+{
+	int num = 25;
+	int* pnum = &num;
+	revers(pnum);
+}
+
+void revers(int *pnum)
+{
+	int num1 = *pnum / 10 % 10;
+	int num2 = *pnum % 10;
+	int num3 = num2 * 10 + num1;
+
+	cout << "Standart num: " << *pnum << endl
+		<< "Reverse num: " << num3 << endl;
+}
+
+
+
+
